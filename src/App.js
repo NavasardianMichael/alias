f76ef7e062f6f689b4073f945a182cfd59a65756
Forlessons8 <mariamsimonyants05@gmail.com>
2023-03-29 16:06:41 +0400

    header done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { TextField } from '@mui/material';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      Alias by Shirakatsy
+      <Header />
+      
+      <TextField id="outlined-basic" label="Team 1 Name" variant="outlined" />
+      <TextField id="outlined-basic" label="Team 2 Name" variant="outlined" />
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import './header.css'
+
+function Header() {
+    return (
+        <div className="header">
+            <h1>Alias</h1>
+            <img src='https://www.pngarts.com/files/10/Blue-Team-Leader-PNG-Image.png' />
+        </div>
+    )
+}
+
+export default Header
